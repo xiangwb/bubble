750195f7faf1c5e6f6970a4b8747dca88c230212
xiangwb <xiangwenbin521@gmail.com>
2020-07-03 18:41:47 +0800

    为课程分类添加索引

--- bubble/models/mongo/item.py
+++ bubble/models/mongo/item.py
@@ -26,7 +26,7 @@ class SubjectCategory(CommonDocument):
             document.path = '{}/{}'.format(document.parent.path, document.parent.name)
 
     meta = {
-        'indexes': [{'fields': ['name', 'path'], 'unique': True, 'sparse': True, 'types': False}, ]
+        'indexes': [{'fields': ['name', 'path'], 'unique': True}, ]
     }
 
 
