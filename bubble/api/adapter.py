efc6132b87fbd03bf8b27f950c18746c6e14a365
xiangwb <xiangwenbin521@gmail.com>
2020-09-02 15:28:02 +0800

    增加requests依赖

--- bubble/api/adapter.py
+++ bubble/api/adapter.py
@@ -3,7 +3,7 @@ from bubble.config import USER_CENTER_URL
 
 
 def get_user(user_id: str) -> dict:
-    url = USER_CENTER_URL + 'api/v1/internal/users/' + user_id
+    url = USER_CENTER_URL + '/api/v1/internal/users/' + user_id
     response = requests.get(url).json()
     code = response.get('code')
     if code != 200:
