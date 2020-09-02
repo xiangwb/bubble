58d4f45ce7ec86f39f2c438e72dbf5aaca3967ae
xiangwb <xiangwenbin521@gmail.com>
2020-09-02 15:54:36 +0800

    修复内部请求url错误的bug

--- bubble/api/adapter.py
+++ bubble/api/adapter.py
@@ -4,7 +4,9 @@ from bubble.config import USER_CENTER_URL
 
 def get_user(user_id: str) -> dict:
     url = USER_CENTER_URL + '/api/v1/internal/users/' + user_id
-    response = requests.get(url).json()
+    headers = {"Content-Type": "application/json"}
+    timeout = 60
+    response = requests.get(url,headers=headers,timeout=timeout).json()
     code = response.get('code')
     if code != 200:
         raise Exception("Not Found")
